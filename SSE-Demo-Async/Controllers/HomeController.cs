89da2d55131c10749359675bd3da302fcee28974
JJ Orozco López <jjorozcodev@gmail.com>
2020-05-10 00:25:14 -0600

    Implementing SSE example with time
    Using Threading and Sleep function.

--- SSE-Demo-Async/Controllers/HomeController.cs
+++ SSE-Demo-Async/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace SSE_Demo_Async.Controllers
@@ -12,5 +10,27 @@ namespace SSE_Demo_Async.Controllers
         {
             return View();
         }
+
+        public void SSE()
+        {
+            Response.ContentType = "text/event-stream";
+
+            DateTime startDate = DateTime.Now;
+            while (startDate.AddMinutes(1) > DateTime.Now)
+            {
+                if (!Response.IsClientConnected)
+                {
+                    break;
+                }
+
+                Response.Write(string.Format("data: {0}\n\n", DateTime.Now.ToString()));
+                Response.Flush();
+
+                System.Threading.Thread.Sleep(3000);
+            }
+            
+            Response.Close();
+        }
+
     }
 }
